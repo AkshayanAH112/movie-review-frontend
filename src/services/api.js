4d781ba25f226ba5aebcf470c439924345143e6c
AkshayanAH112 <akshayan078@gmail.com>
2025-04-04 15:12:01 +0530

    new backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class ApiService {
-  static BASE_URL = "http://13.200.236.137/api";
+  static BASE_URL = "http://13.235.214.33/api";
 
   static getHeader() {
     const token = localStorage.getItem("token");
